f12c72c1e5870676a78c6411aea9e28f41d668a0
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-31 00:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
 import streamlit as st
-from transformers import Tacotron2ForTextToSpeech, Tacotron2Processor
 import torch
 import soundfile as sf
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    processor = Tacotron2Processor.from_pretrained("tugstugi/tacotron2")
-    model = Tacotron2ForTextToSpeech.from_pretrained("tugstugi/tacotron2")
-    return processor, model
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
 
-def generate_speech(text, processor, model):
-    inputs = processor(text, return_tensors="pt")
-    with torch.no_grad():
-        model.eval()
-        speech = model.generate(inputs.input_ids)
-    return speech[0].numpy()
+def generate_audio(text):
+    inputs = tokenizer.encode(text, return_tensors="pt")
+    outputs = model.generate(inputs)
+    speech = outputs[0].numpy()
+    return speech
 
 def main():
     st.title("Text-to-Speech Demo")
     text = st.text_input("Enter text")
 
-    # Load the model
-    processor, model = load_model()
-
     if st.button("Generate Audio"):
-        speech = generate_speech(text, processor, model)
-        sf.write("speech.wav", speech, samplerate=22050)
-        st.audio("speech.wav", format="audio/wav")
+        wav = generate_audio(text)
+        sf.write("speech.wav", wav, samplerate=22050)
+        st.success("Audio generated successfully. You can download it below.")
+        st.audio("speech.wav")
 
 if __name__ == "__main__":
     main()
