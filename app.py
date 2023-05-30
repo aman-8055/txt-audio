2e9be697cd70f8d421bd809fb7f0742288e258a1
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-31 00:38:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
 import streamlit as st
 import torch
-import soundfile as sf
-from transformers import AutoTokenizer, AutoModelForTextToSpeech
-
-# Load tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("tts_models/ljspeech")
-model = AutoModelForTextToSpeech.from_pretrained("tts_models/ljspeech")
+from transformers import Tacotron2ForTextToSpeech, Tacotron2Processor
+from pydub import AudioSegment
 
 def generate_audio(text):
-    # Encode input text
-    input_ids = tokenizer.encode(text, return_tensors="pt")
+    # Load Tacotron2 model and processor
+    processor = Tacotron2Processor.from_pretrained("tugstugi/tacotron2")
+    model = Tacotron2ForTextToSpeech.from_pretrained("tugstugi/tacotron2")
 
-    # Generate audio
+    # Generate speech from text
+    inputs = processor(text, return_tensors="pt", padding=True)
     with torch.no_grad():
-        output = model.generate(input_ids)
-
-    # Save audio to file
-    audio = output[0].numpy()
-    sf.write("output.wav", audio, samplerate=22050)
+        wav = model.generate(inputs["input_ids"], inputs["attention_mask"])
+    
+    return wav
 
 def main():
     st.title("Text-to-Speech Demo")
     text = st.text_input("Enter text")
 
     if st.button("Generate Audio"):
-        generate_audio(text)
+        wav = generate_audio(text)
+        audio = AudioSegment.from_wav(wav.tostring())
+
+        # Save the audio to a file
+        audio.export("output.wav", format="wav")
+
+        # Play the audio in the Streamlit app
         st.audio("output.wav")
 
 if __name__ == "__main__":
