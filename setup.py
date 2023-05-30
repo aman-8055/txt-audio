0c2e4c3e036c01a4a25a3629f8f64ca24bb6fc10
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-31 00:34:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'streamlit==0.85.1',
+        'streamlit==0.89.0',
         'torch==1.9.0',
         'soundfile==0.10.3.post1',
         'transformers==4.10.3',
@@ -13,15 +13,6 @@ setup(
     entry_points={
         'console_scripts': [
             'txt-audio=app:main',
-        ],
-    },
-    setup_requires=[
-        'numpy',
-        'Cython',
-    ],
-    extras_require={
-        'dev': [
-            'pytest',
         ],
     },
 )
