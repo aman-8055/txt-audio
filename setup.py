db865a8a097b345ebe745eae15fe13791746ff14
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-30 23:40:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'streamlit',
-        'fairseq',
-        'huggingface_hub',
-        'IPython',
+        'streamlit==0.87.0',
+        'fairseq==0.10.2',
+        'huggingface_hub==0.0.12',
+        'IPython==7.33.0',
         'tensorboardX',
     ],
     entry_points={
