da457e63ae7e9a09182af571b86ba3075bcf9705
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-30 23:11:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='streamlit_tts_demo',
+    name='txt-audio',
     version='1.0',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
-        'fairseq',
-        'streamlit',
-        'ipython'
+        'streamlit==0.87.0',
+        'fairseq==0.10.2',
+        'huggingface_hub==0.0.12',
+        'IPython==7.33.0',
     ],
     entry_points={
         'console_scripts': [
-            'streamlit_tts_demo = app:main'
-        ]
+            'txt-audio = app:main',
+        ],
     },
 )
