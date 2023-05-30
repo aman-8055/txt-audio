4bfa859035ee70b4d3d49bf3bd892d5e8f1d3cf1
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-31 00:39:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'streamlit==0.89.0',
+        'streamlit==0.85.1',
         'torch==1.9.0',
-        'soundfile==0.10.3.post1',
         'transformers==4.10.3',
+        'pydub==0.25.1',
     ],
     entry_points={
         'console_scripts': [
